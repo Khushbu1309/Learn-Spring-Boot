3199d7c8854f5898e97fb7969cee4b0a2ffdcbf2
Khushbu1309 <khushbubora711@gmail.com>
2023-03-11 15:10:36 +0530

    Welcome to Spring Boot

--- LearnSpringBoot/src/main/java/com/example/demo/LearnSpringBootApplication.java
+++ LearnSpringBoot/src/main/java/com/example/demo/LearnSpringBootApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,14 @@ public class LearnSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearnSpringBootApplication.class, args);
+		System.out.println("Welcome to Learn Spring Boot");
+		
+//		 SpringApplication app = new SpringApplication(LearnSpringBootApplication.class);
+//	        app.setDefaultProperties(Collections
+//	          .singletonMap("server.port", "8083"));
+//	        app.run(args);
+
+		
 	}
 
 }
