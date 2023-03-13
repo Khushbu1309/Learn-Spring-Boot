9c8c3830c09fddf2d5f63cf0eabcd1c17a6c707c
Khushbu1309 <khushbubora711@gmail.com>
2023-03-13 22:17:20 +0530

    call consructor using prehand object provided by spring  singleton design pattern

--- LearnSpringBoot/src/main/java/com/example/demo/LearnSpringBootApplication.java
+++ LearnSpringBoot/src/main/java/com/example/demo/LearnSpringBootApplication.java
@@ -10,16 +10,38 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class LearnSpringBootApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(LearnSpringBootApplication.class, args);
-//		System.out.println("Welcome to Learn Spring Boot");
-		ConfigurableApplicationContext context=SpringApplication.run(LearnSpringBootApplication.class, args); 
-		Student_Info obj=context.getBean(Student_Info.class);
-		obj.show();
-//		 SpringApplication app = new SpringApplication(LearnSpringBootApplication.class);
-//	        app.setDefaultProperties(Collections
-//	          .singletonMap("server.port", "8083"));
-//	        app.run(args);
-
+		
+		//Start Spring Boot
+		/*
+		 * SpringApplication.run(LearnSpringBootApplication.class, args);
+		 * System.out.println("Welcome to Learn Spring Boot");
+		 */
+		
+		
+		//call a other class function using @Component object creation 
+		
+		
+		/*
+		 * ConfigurableApplicationContext
+		 * context=SpringApplication.run(LearnSpringBootApplication.class, args);
+		 * Student_Info obj=context.getBean(Student_Info.class); obj.show();
+		 */
+		 
+		 // call consructor using prehand object provided by spring  singleton design pattern 
+		  ConfigurableApplicationContext
+		  context=SpringApplication.run(LearnSpringBootApplication.class, args);
+		
+		
+		
+		
+		
+		//Port change code
+		/*
+		 * SpringApplication app = new
+		 * SpringApplication(LearnSpringBootApplication.class);
+		 * app.setDefaultProperties(Collections .singletonMap("server.port", "8083"));
+		 * app.run(args);
+		 */
 		
 	}
 
